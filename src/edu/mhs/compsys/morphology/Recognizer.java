f778bec7c7c5c3c171da0ee262924a2883a49baa
Akshay K Ramkumar <akshay.karthik@gmail.com>
2013-01-08 11:59:48 -0500

    Basic recognizer working.

--- src/edu/mhs/compsys/morphology/Recognizer.java
+++ src/edu/mhs/compsys/morphology/Recognizer.java
@@ -1,5 +1,8 @@
 package edu.mhs.compsys.morphology;
 
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
 import edu.mhs.compsys.idt.Dataset;
 import edu.mhs.compsys.reporting.Report;
 
@@ -7,9 +10,26 @@ public class Recognizer {
 
 	private Dataset data;
 	private Report report;
-	
-	public Recognizer() {
-		
+	private ArrayList<BufferedImage> changes;
+
+	public Recognizer(Dataset data) {
+		this.data = data;
+		this.report = new Report();
 	}
 
+	public void process() {
+		for (int i = 0; i < data.length() - 1; i++) {
+			changes.add(BinaryImageProcessor.toImage(BinaryImageProcessor
+					.fromDiff(data.get(i), data.get(i + 1))));
+		}
+
+	}
+
+	public Report getReport() {
+		return report;
+	}
+
+	public ArrayList<BufferedImage> getChanges() {
+		return changes;
+	}
 }
